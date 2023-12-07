7b7bfe695a319af5902d543a111249995aaca335
vanthao <@RonaldodeLema>
2023-12-07 12:29:37 +0700

    feat: add swagger

--- website/src/application/routes/chainRoute.js
+++ website/src/application/routes/chainRoute.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const chainController = require('../controllers/chainController');
 
 router.get('/', chainController.getChain);
+router.get('/user', chainController.getAllUsers);
+// router.post('/user', chainController.createUser);
+// router.get('/user/${id}', chainController.findUserById);
+router.get('/offer', chainController.getAllOffers);
 
 module.exports = router;
