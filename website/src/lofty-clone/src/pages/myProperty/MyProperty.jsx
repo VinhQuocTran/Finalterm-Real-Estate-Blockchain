bb8ae9c2d291610ac59c499fb431617a32e841aa
Tran Tri <tranminhtri0253@gmail.com>
2023-12-19 12:53:38 +0700

    fix: change property field on ui

--- website/src/lofty-clone/src/pages/myProperty/MyProperty.jsx
+++ website/src/lofty-clone/src/pages/myProperty/MyProperty.jsx
@@ -215,17 +215,19 @@ const MyProperty = () => {
         }
       });
 
-      dispatch(updateUserProperties(response.data.data));
-      toast.success('Create new property successfully', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+      if (response.data.status === "success") {
+        dispatch(updateUserProperties(response.data.data));
+        toast.success('Create new property successfully', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      }
 
       // reset form
       setValues({
